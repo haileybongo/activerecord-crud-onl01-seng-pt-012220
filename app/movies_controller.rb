d994bbccdc33f6861bae1612a791e36b9cf54ea7
Hailey Bongo <haileyebongo@gmail.com>
2020-04-08 20:03:29 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,8 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
   
   Movie.create do |m|
-    m.title = self.title
+    m.title = args.value 
+    binding.pry
     m.release_date = @release_date
       end
 end
